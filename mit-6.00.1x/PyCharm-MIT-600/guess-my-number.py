20539e554cff1067bc93e05de78a173943742bfb
andrekorol <anrobits@yahoo.com.br>
2016-10-06 00:37:42 -0300

    Novos arquivos do curso do MIT-Novos arquivos Week 2 . 4

--- mit-6.00.1x/PyCharm-MIT-600/guess-my-number.py
+++ mit-6.00.1x/PyCharm-MIT-600/guess-my-number.py
@@ -1,24 +1,31 @@
 print("Please think of a number between 0 and 100!")
 
 low = 0
-
+medium = 50
 high = 100
 
-while True:
-    my_guess = (high + low) / 2
-    print("Is your secret number %d?" % my_guess)
-    ans = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter "
+print("Is your secret number %d?" % medium)
+
+ans = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter "
                 "'c' to indicate I guessed correctly.")
+while True:
     if ans == 'h':
-        high = my_guess
+        high = medium
+        medium = int((medium + low) / 2)
 
     elif ans == 'l':
-        low = my_guess
+        low = medium
+        medium = int((medium+ high) / 2)
 
     elif ans == 'c':
         break
 
     else:
         print("Sorry, I did not understand your input.")
+    print("Is your secret number %d?" % medium)
 
-print("Game over. Your secret number was: %d" % my_guess)
+    ans = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter "
+                "'c' to indicate I guessed correctly.")
+
+
+print("Game over. Your secret number was: %d" % medium)
---
+++ mit-6.00.1x/PyCharm-MIT-600/var-scope-test.py
@@ -0,0 +1,7 @@
+def f(x):
+    x += 1
+    print('in f(x): x = ', x)
+    return x
+
+x = 3
+z = f(x)
