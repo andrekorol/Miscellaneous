6c852c6d47fb0edb8116d5d95f3983d81a4c6e39
andrekorol <anrobits@yahoo.com.br>
2017-05-16 21:21:22 -0300

    Finish "Lista Vetor 2"

--- 1Introdução à Computação/andrekorol/Lista Vetor 1/ex4/ex4.c
+++ 1Introdução à Computação/andrekorol/Lista Vetor 1/ex4/ex4.c
@@ -5,8 +5,8 @@
 #define MAX 100
 void descending_bubble_sort(int vet[], int max) {
     int i, j, aux;
-    for( i = 0; i < max; i++) {
-	for(j = 0; j < (max - 1); j++) {
+    for (i = 0; i < max; i++) {
+	for (j = 0; j < (max - 1); j++) {
 	    if (vet[j] < vet[j+1]) {
 		aux = vet[j+1];
 		vet[j+1] = vet[j];
