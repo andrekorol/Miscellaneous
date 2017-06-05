a5021dd0383ff505d78908529f0d019661ab479c
andrekorol <anrobits@yahoo.com.br>
2017-06-04 21:22:49 -0300

    Finish problem811A and start problem811B

---
+++ competition/code-forces/811/problem811B.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+    int n, m;
+    scanf(" %d %d", &n, &m);
+    int P[n], answers[m];
+    for (int i = 0; i < n; ++i)
+        scanf(" %d", &P[i]);
+
+    return 0;
+}
